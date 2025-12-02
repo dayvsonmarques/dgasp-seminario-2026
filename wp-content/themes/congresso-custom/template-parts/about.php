8b7bac30be3a4e4d5fd2a46e343a39fb5f926ef1
Dayvson Marques <dayvson.marques@gmail.com>
2025-12-02 15:12:36 -0300

    issue: scss nesting

--- wp-content/themes/congresso-custom/template-parts/about.php
+++ wp-content/themes/congresso-custom/template-parts/about.php
@@ -7,14 +7,14 @@
             <div class="col-lg-9">
                 <div class="row">
                     <div class="col-md-6">
-                        <div class="card event-card h-100">
+                        <div class="card event-card h-100 p-5">
                             <h3 class="card-title">III SEMINÁRIO ESTADUAL DE ATENÇÃO À SAÚDE PRISIONAL DE PERNAMBUCO</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
                             <a href="#" class="btn btn-primary btn-learn">SAIBA MAIS</a>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div class="card event-card h-100">
+                        <div class="card event-card h-100 p-5">
                             <h3 class="card-title">III MOSTRA ESTADUAL DE EXPERIÊNCIAS NA SAÚDE PRISIONAL</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
                             <a href="#" class="btn btn-primary btn-topics">EIXOS TEMÁTICOS</a>
