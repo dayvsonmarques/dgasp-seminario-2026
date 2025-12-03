1c13c6a281edac8e308ea6a172fff43007e55f01
Dayvson Marques <dayvso.marques@gmail.com>
2025-12-03 15:53:14 -0300

    feature: participatin implemented and css comments has been removed

--- wp-content/themes/congresso-custom/template-parts/participation.php
+++ wp-content/themes/congresso-custom/template-parts/participation.php
@@ -1,11 +1,11 @@
-<section class="participation py-5 bg-white">
+<section class="section-participation bg-white">
     <div class="container text-center">
-        <h2 class="section-title text-blue">
+        <h2 class="section-title text-blue mb-1">
             <span class="bold">PARTICIPE</span>
             <span class="light">DO </span>
             <span class="bold">SEMINÁRIO</span>
         </h2>
-        <p class="text-dark-blue">Inscreva-se e faça parte dessa jornada de saúde, integração e cuidado.</p>
+        <p class="text-dark-blue h4">Inscreva-se e faça parte dessa jornada de saúde, integração e cuidado.</p>
         <a href="#" class="btn btn-primary btn-register">CADASTRE-SE AQUI</a>
     </div>
 </section>
